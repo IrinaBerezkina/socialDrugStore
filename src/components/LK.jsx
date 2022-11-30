c45949d4f3d3c5fe1b0985c953588edaf10b055a
Victoria <viktoriya.zapisochnaya@gmail.com>
2022-11-30 15:52:18 +0300

    releaseMainPageRFCAdded

--- src/components/LK.jsx
+++ src/components/LK.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 
 export default function LK() {
   return (
-    <div>LK</div>
+    <div className="container">
+      <div className="card" style={{ width: '18rem' }}>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            Login
+            <button type="button">Edit</button>
+
+          </li>
+          <li className="list-group-item">
+
+            Email Address
+            <button type="button">Edit</button>
+          </li>
+
+        </ul>
+      </div>
+    </div>
   );
 }
