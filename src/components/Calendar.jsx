50e45ef283eeb3e40a71859e5f8e258936daf556
IrinaBerezkina <ira1berez@gmail.com>
2022-11-30 17:32:53 +0300

    calendar

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+export default function Calendar() {
+  const [data, setData] = useState({ data: '' });
+  const changeHandler = (e) => {
+    setData({ [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="form-group">
+      <h4 className="heading">Choose date for free medicine</h4>
+      <div className="grid">
+        <div className="col-1-4 col-1-4-sm">
+          <div className="controls">
+            <input
+              type="date"
+              onChange={(e) => changeHandler(e)}
+              id="arrive"
+              className="floatLabel"
+              name="data"
+              value={data.data}
+            />
+            {!data.data
+              ? (
+                <label htmlFor="arrive" className="label-date">
+                  <i className="fa fa-calendar" />
+                  Data
+                </label>
+              )
+              : (
+                <label htmlFor="arrive" className="label-date">
+                  <i className="fa fa-calendar" />
+
+                  {data.data}
+                </label>
+              )}
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
